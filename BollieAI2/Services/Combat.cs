ec52380d97dcbc16ecc8033b34cfd7ee2563b747
Bolukan <github@bolukan.nl>
2014-12-21 16:56:56 +0100

    Finalised Combat

--- BollieAI2/Services/Combat.cs
+++ BollieAI2/Services/Combat.cs
@@ -10,6 +10,58 @@ namespace BollieAI2.Services
     /// </summary>
     public class Combat
     {
+        #region quick
+
+        // 95% or more success
+        private static int[] AttackersNeededSmall = { 0, 
+                     2, 3, 5, 7, 8, 10, 12, 14, 15, 17, 
+                     19, 21, 22, 24, 26, 27, 29, 31, 32, 34,
+                     36, 38, 39, 41, 43, 44, 46, 48, 49, 51,
+                     53, 55, 56, 58, 60, 61, 63, 65, 66, 68};
+
+        // 95% or more confidence max loss attacker killed by defenders
+        private static int[] AttackersLossesSmall = { 0,
+                     1, 1, 2, 3, 4, 4, 5, 6, 7, 7,
+                     8, 9, 10, 10, 11, 12, 12, 13, 14, 14,
+                     15, 16, 17, 17, 18, 19, 20, 20, 21, 22,
+                     22, 23, 24, 24, 25, 26, 27, 27, 28, 29};
+
+        /// <summary>
+        /// Number of attackers needed to kill all defenders with 95% or more confidence
+        /// </summary>
+        /// <param name="defenders"># defenders</param>
+        /// <returns># attackers needed</returns>
+        public static int AttackersNeeded(int defenders)
+        {
+            if (defenders > 40)
+            {
+                return (int)(0.5 + (defenders + 1) * 0.71D);
+            }
+            else
+            {
+                return AttackersNeededSmall[defenders];
+            }
+        }
+
+        /// <summary>
+        /// Number of attackers at 95% confidence (so kind of max value) that will be killed by defenders
+        /// </summary>
+        /// <param name="defenders"># defenders</param>
+        /// <returns># attackers killed</returns>
+        public static int AttackersLosses(int defenders)
+        {
+            if (defenders > 40)
+            {
+                return (int)(0.5 + (defenders + 1) * 5 / 3);
+            }
+            else
+            {
+                return AttackersLossesSmall[defenders];
+            }
+        }
+
+        #endregion
+
         public const double LUCK = 0.16;
 
         public const double ATTACKERS_CHANCE = 0.6;
@@ -21,177 +73,77 @@ namespace BollieAI2.Services
         public enum ChancesType
         {
             Attacker = 0,
-            Defender,
-            AttackerCum,
-            DefenderCum
+            Defender = 1
         }
-
-        // 95% or more success
-        private static int[] AttackersNeededSmall = { 0, 2, 3, 5, 7, 9, 10, 12, 14, 15, 17, 19, 21, 22, 24, 26, 27, 29 };
-        public static int AttackersNeeded(int defenders)
-        {
-            if (defenders > 17)
-            {
-                // this is not scientific
-                return (AttackersNeededSmall[17] + (int)((((defenders-17)*5)+1.7D)/3));
-            }
-            else
-            {
-                return AttackersNeededSmall[defenders];
-            }
-        }
-
-        private static Dictionary<int, Dictionary<int, double[]>> Chances;
-
-
-        private static Dictionary<int, Dictionary<int, double>> AttackChances;
-        private static Dictionary<int, Dictionary<int, double>> DefendChances;
-        private static int AttackChancesMax = 0;
-        private static int DefendChancesMax = 0;
+        
+        // Attack, Defend
+        // #armies (1 .. n)
+        // #outcome (0 .. #armies)
+        // chance single/cumulative (=cum(single_outcome(0..outcome))
+        private static Dictionary<ChancesType, Dictionary<int, Dictionary<int, double[]>>> Chances =
+             new Dictionary<ChancesType, Dictionary<int, Dictionary<int, double[]>>>(2);
         
         /// <summary>
         /// 
         /// </summary>
         /// <param name="armiesAttackers"></param>
         /// <returns>chances of result</returns>
-        public static Dictionary<int, double> GetAttackChances(int armiesAttackers)
+        public static Dictionary<int, double[]> GetChances(ChancesType chancesType, int armiesIngame)
         {
-            while (armiesAttackers > AttackChancesMax)
+            // init Chance
+            double Chance;
+            if (chancesType == ChancesType.Attacker) Chance = ATTACKERS_CHANCE; else Chance = DEFENDERS_CHANCE;
+
+            // Count number existing
+            if (!Chances.ContainsKey(chancesType))
             {
-                AttackChancesMax++;
-                Dictionary<int, double> attackChances = new Dictionary<int, double>(AttackChancesMax + 1);
-                // Calculate chances
-                if (AttackChancesMax == 1)
+                Chances.Add(chancesType, new Dictionary<int, Dictionary<int, double[]>>(armiesIngame));
+            }
+
+            int armiesMax = Chances[chancesType].Count();
+
+           
+            // Add new calculations till armies reached
+            while (armiesMax < armiesIngame)
+            {
+                // create next outcome 'column'
+                armiesMax++;
+                
+                // Create outcome variable: outcome is 0 .. # armies, which is 1 more than # armies
+                Dictionary<int, double[]> outcome = new Dictionary<int, double[]>(armiesMax + 1);
+                
+                // 1 army is just the Chance
+                if (armiesMax == 1)
                 {
-                    AttackChances = new Dictionary<int, Dictionary<int, double>>();
-                    attackChances.Add(0, 1D - ATTACKERS_CHANCE);
-                    attackChances.Add(1, ATTACKERS_CHANCE);
+                    outcome.Add(0, new double[2] { 1-Chance, 1-Chance });
+                    outcome.Add(1, new double[2] { Chance, 1 });
                 }
                 else
                 {
-                    // again a loss
-                    attackChances.Add(0, (1D - ATTACKERS_CHANCE) * AttackChances[AttackChancesMax-1][0]);
-                    // a loss + a win
-                    for (int armies = 1; armies < AttackChancesMax; armies++)
+                    // calculations use results from 'armies-1'
+                    Dictionary<int, double[]> priorOutcome = Chances[chancesType][armiesMax-1];
+                    
+                    // all losses (outcome still '0')
+                    int armies = 0;
+                    double singleChance = (1 - Chance) * priorOutcome[armies][0]; // [0] Single outcome
+                    outcome.Add(0, new double[2] { singleChance, singleChance } );
+                    // loss + win
+                    for (armies = 1; armies < armiesMax; armies++)
                     {
-                        attackChances.Add(armies, (1D - ATTACKERS_CHANCE) * AttackChances[AttackChancesMax - 1][armies]
-                            + ATTACKERS_CHANCE * AttackChances[AttackChancesMax - 1][armies-1]);
+                        singleChance = (1-Chance) * priorOutcome[armies][0] + Chance * priorOutcome[armies-1][0]; // [0] single
+                        outcome.Add(armies, new double[2] { singleChance, singleChance + outcome[armies-1][1] }); // [1] cumulative
                     }
-                    // win only
-                    attackChances.Add(AttackChancesMax, ATTACKERS_CHANCE * AttackChances[AttackChancesMax - 1][AttackChancesMax - 1]);
+                    // all wins
+                    outcome.Add(armiesMax, new double[2] { Chance * priorOutcome[armiesMax-1][0], 1 });
                 }
-                // add to memory
-                AttackChances.Add(AttackChancesMax, attackChances);
-            }
-            // return from info
-            return AttackChances[armiesAttackers];
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="armiesDefenders"></param>
-        /// <returns>chances of result</returns>
-        public static Dictionary<int, double> GetDefendChances(int armiesDefenders)
-        {
-            while (armiesDefenders > DefendChancesMax)
-            {
-                DefendChancesMax++;
-                Dictionary<int, double> defendChances = new Dictionary<int, double>(DefendChancesMax + 1);
-                // Calculate chances
-                if (DefendChancesMax == 1)
-                {
-                    DefendChances = new Dictionary<int, Dictionary<int, double>>();
-                    defendChances.Add(0, 1D - DEFENDERS_CHANCE);
-                    defendChances.Add(1, DEFENDERS_CHANCE);
-                }
-                else
-                {
-                    // again a loss
-                    defendChances.Add(0, (1D - DEFENDERS_CHANCE) * DefendChances[DefendChancesMax - 1][0]);
-                    // a loss + a win
-                    for (int armies = 1; armies < DefendChancesMax; armies++)
-                    {
-                        defendChances.Add(armies, (1D - DEFENDERS_CHANCE) * DefendChances[DefendChancesMax - 1][armies]
-                            + DEFENDERS_CHANCE * DefendChances[DefendChancesMax - 1][armies - 1]);
-                    }
-                    // a win
-                    defendChances.Add(DefendChancesMax, DEFENDERS_CHANCE * DefendChances[DefendChancesMax - 1][DefendChancesMax - 1]);
-                }
-                // add to memory
-                DefendChances.Add(DefendChancesMax, defendChances);
-            }
-            // return from info
-            return DefendChances[armiesDefenders];
-        }
-
-/*
-        public static void test()
-        {
-            CombatResult cr;
-            for (int d = 1; d <= 30; d++)
-            {
-                cr = CalcCombatWorst(AttackersNeededBadLuck(d), d);
-                Console.WriteLine("Need {0} for {1} defenders, {2} survive, {3} die", cr.Attackers, cr.Defenders, cr.AttackersLeft, cr.Attackers - cr.AttackersLeft );
-            }
-        }
-*/
-        public static int AttackersNeededBadLuck(int defenders)
-        {
-            int attackers = (int)(0.5 + (defenders * DEFENDERS_LUCK)) + 1;
-            while (Math.Round(attackers * ATTACKERS_LUCK * (1 - LUCK), 0) < defenders)
-                attackers++;
-            return attackers;
-        }
-/*
-        public static CombatResult CalcCombatWorst(int attackers, int defenders)
-        {
-            CombatResult combatResult = new CombatResult();
-            combatResult.Attackers = attackers;
-            combatResult.Defenders = defenders;
-
-            int defendersCanBeDestroyed = (int)(0.5 + (attackers * ATTACKERS_LUCK * (1 - LUCK)));
-            int attackersCanBeDestroyed = (int)(0.5 + (defenders * DEFENDERS_LUCK));
             
-            // attackers left
-            if (attackers > attackersCanBeDestroyed)
-            {
-                // defenders gone
-                if (defenders > defendersCanBeDestroyed)
-                {
-                    // We survived, but also they did
-                    combatResult.AttackWins = false;
-                    combatResult.AttackersLeft = attackers - attackersCanBeDestroyed;
-                    combatResult.DefendersLeft = defenders - defendersCanBeDestroyed;
-                }
-                else
-                {
-                    // YEAH WE WIN !!
-                    combatResult.AttackWins = true;
-                    combatResult.AttackersLeft = attackers - attackersCanBeDestroyed;
-                    combatResult.DefendersLeft = 0;
-                }
-            }
-            else
-            {
-                // oops too many
-                combatResult.AttackWins = false;
-                combatResult.AttackersLeft = 0;
-                // a few or more survived
-                if (defendersCanBeDestroyed < defenders)
-                {
-                    combatResult.DefendersLeft = defenders - defendersCanBeDestroyed;
-                }
-                else
-                {
-                    // if only one of us had survived to see them all killed. One will stay.
-                    combatResult.DefendersLeft = 1;
-                }
+                // Add this to memory
+                Chances[chancesType].Add(armiesMax, outcome);
             }
 
-            return combatResult;
-
+            // return from info
+            return Chances[chancesType][armiesIngame];
         }
-*/
+     
     }
 }
